106a45e310632b646f238f7c588b98a91798a5fd
Michal Harakal <michal.harakal@googlemail.com>
2025-05-17 21:40:06 +0200

    Improve publishing as github artifact

--- kotlin-jupyter/build.gradle.kts
+++ kotlin-jupyter/build.gradle.kts
@@ -18,13 +18,13 @@ kotlin {
 
 tasks.shadowJar {
     archiveClassifier.set("")
-    
+
     // Only include sk.ai.net classes
     relocate("sk.ai.net", "sk.ai.net")
     minimize {
         exclude(dependency("sk.ai.net:.*:.*"))
     }
-    
+
     // Exclude everything else
     dependencies {
         exclude { it.moduleGroup != "sk.ai.net" }
@@ -38,14 +38,44 @@ publishing {
             groupId = "sk.ai.net"
             artifactId = "kotlin-jupyter"
             version = "0.0.6-SNAPSHOT"
-            
+
             // Use only the shadow jar
             artifact(tasks.shadowJar.get()) {
                 classifier = null
             }
+            pom {
+                description.set("skainet-jupyter")
+                name.set(project.name)
+                url.set("https://github.com/sk-ai-net/skainet-jupyter/")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/sk-ai-net/skainet-jupyter/")
+                    connection.set("scm:git:git@github.com:sk-ai-net/skainet-jupyter.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:sk-ai-net/skainet-jupyter.git")
+                }
+                developers {
+                    developer {
+                        id.set("sk-ai-net")
+                        name.set("sk-ai-net")
+                    }
+                }
+            }
         }
+
     }
+
     repositories {
-        mavenLocal()
+        maven {
+            name = "githubPackages"
+            url = uri("https://maven.pkg.github.com/sk-ai-net/skainet")
+            credentials {
+                credentials(PasswordCredentials::class)
+            }
+        }
     }
 }
