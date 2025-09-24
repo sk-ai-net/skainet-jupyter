9afee4272309308265a2118e435ccc6bda3f69f4
Michal Harakal <michal.harakal@googlemail.com>
2025-09-24 21:56:10 +0200

    Add back gradle warper, rename module to skainet-kotlin-notebook

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "skainet-notebook"
+
+include("skainet-kotlin-notebook")
---
+++ skainet-kotlin-notebook/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.jetbrainsKotlinJvm)
+    `maven-publish`
+    alias(libs.plugins.johnrengelman.shadow)
+}
+
+dependencies {
+    implementation(libs.skainet.api)
+    implementation(libs.skainet.core)
+    implementation(libs.skainet.nn)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+
+    // Include sk.ainet.core dependencies
+    dependencies {
+        include(dependency("sk.ainet.core:.*:.*"))
+    }
+}
+
+// Configure publishing
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "sk.ainet"
+            artifactId = "kotlin-notebook"
+            version = "0.0.1"
+
+            // Use only the shadow jar
+            artifact(tasks.shadowJar.get()) {
+                classifier = null
+            }
+            pom {
+                description.set("skainet-notebook")
+                name.set(project.name)
+                url.set("https://github.com/sk-ai-net/skainet-jupyter/")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        distribution.set("repo")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/sk-ai-net/skainet-jupyter/")
+                    connection.set("scm:git:git@github.com:sk-ai-net/skainet-jupyter.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:sk-ai-net/skainet-jupyter.git")
+                }
+                developers {
+                    developer {
+                        id.set("sk-ai-net")
+                        name.set("sk-ai-net")
+                    }
+                }
+            }
+        }
+
+    }
+}
